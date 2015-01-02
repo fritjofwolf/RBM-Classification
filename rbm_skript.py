af5e6e0707cfb9e5dca4c1da87110d2dd30834bb
Katarzyna Tarnowska <Katarzyna Tarnowska@KT>
2015-01-02 03:36:27 +0100

    +import

--- rbm_skript.py
+++ rbm_skript.py
@@ -3,8 +3,8 @@ from data import *
 import math
 import numpy as np
 
-RBM1 = RBM(5,2)
-print RBM1.Weights
+#RBM1 = RBM(5,2)
+#print RBM1.Weights
 
 #Test on loading, binarizing/scaling and printing/saving MNIST train data
 printData(binarizeH(readData("mnist_train.csv", n=100)));
