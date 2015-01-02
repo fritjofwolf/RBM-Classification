d790d1562604ada4c036dfe6cda4256850b0fb9d
Katarzyna Tarnowska <Katarzyna Tarnowska@KT>
2015-01-02 03:13:49 +0100

    Testing functions on data

--- rbm_skript.py
+++ rbm_skript.py
@@ -4,3 +4,7 @@ import numpy as np
 
 RBM1 = RBM(5,2)
 print RBM1.Weights
+
+#Test on loading, binarizing/scaling and printing/saving MNIST train data
+printData(binarizeH(readData("mnist_train.csv", n=100)));
+#saveData("out.csv", binarizeH(readData("mnist_train.csv", n=100)));
