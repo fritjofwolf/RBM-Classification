01a03c372ebbefe8d95942d036811f9dce19ccd4
Katarzyna Tarnowska <Katarzyna Tarnowska@10.1.8.123>
2015-01-16 19:10:09 +0100

    Begin working on inheritance

--- RBM.py
+++ RBM.py
@@ -91,4 +91,93 @@ class RestrictedBoltzmannMachine(object):
 					visible[i] = 0
 					
 		return visible
-		
+	
+    
+class Discriminative(RestrictedBoltzmannMachine):
+    def __init__(self, numOfVisibleUnits, numOfHiddenUnits, numOfTargetUnits, weightsVH = [], weightsTH = [], scal = 0.01, bin = True):
+        RBM.__init__(self, numOfVisibleUnits, numOfHiddenUnits, scal, bin)
+        #self.NumOfVisibleUnits = numOfVisibleUnits
+        #self.NumOfHiddenUnits = numOfHiddenUnits
+        self.NumOfTargetUnits = numOfTargetUnits
+        
+        #Set the visible biases to log[pi/ (1-pi)] where pi is the proportion of training vectors in which unit i is on.
+        self.VisibleBiases = np.random.random(numOfVisibleUnits)
+        
+        #Set the hidden biases to 0.
+        self.HiddenBiases = np.random.random(numOfHiddenUnits)
+        
+        self.TargetBiases = np.random.random(numOfTargetUnits)
+        
+        #Initialize weights
+        # Use small random values for the weights chosen from a zero-mean Gaussian with a standard deviation of 0.01.
+        if weightsVH != []: # weights between visible and hidden units
+            self.WeightsVH = weightsVH
+        else:
+            #self.WeightsVH = np.random.random([numOfVisibleUnits, numOfHiddenUnits])
+            self.WeightsVH = scal * np.random.randn(numOfVisibleUnits, numOfHiddenUnits)
+        if weightsTH != []: # weights between target and hidden units
+            self.WeightsTH = weightsTH
+        else:
+            #self.WeightsTH = np.random.random([numOfTargetUnits, numOfHiddenUnits])
+            self.WeightsVH = 0.01 * np.random.randn(numOfVisibleUnits, numOfHiddenUnits)
+    
+    # TODO: ANpassen        
+    # Train the RBM using the contrastive divergence algorithm generalized to input and target
+    # Input: trainingData - matrix of training examples, where each row represents an example
+    def train(self, trainingData, learningRate, errorThreshold):
+        counter = 0
+        error = 10000
+        while error > errorThreshold:
+            visible = trainingData[counter]
+            hidden = np.zeros((self.NumOfHiddenUnits,1))
+            visibleRecon = np.zeros((self.NumOfVisibleUnits,1))
+            hiddenRecon = np.zeros((self.NumOfHiddenUnits,1))
+                
+            # Gibbs-Sampling
+            for i in range(self.NumOfHiddenUnits):
+                if np.random.random() < sigmoid(self.HiddenBiases[i] + sum(visible*self.Weights[i,:])):
+                    hidden[i] = 1
+                else:
+                    hidden[i] = 0
+            for i in range(self.NumOfVisibleUnits):
+                if np.random.random() < sigmoid(self.VisibleBiases[i] + sum(hidden*self.Weights[i,:])):
+                    visibleRecon[i] = 1
+                else:
+                    visibleRecon[i] = 0        
+                for i in range(self.NumOfHiddenUnits):
+                    if np.random.random() < sigmoid(self.HiddenBiases[i] + sum(visibleRecon*self.Weights[i,:])):
+                        hiddenRecon[i] = 1
+                    else:
+                        hiddenRecon[i] = 0
+                    
+                # Update weights and biases
+                self.Weights += learningRate * (np.outer(visible,hidden) - np.outer(visibleRecon,hiddenRecon))
+                self.HiddenBiases += learningRate * (hidden - hiddenRecon)
+                self.VisibleBiases += learningRate * (visible - visibleRecon)
+                
+                # Squared-error serves as indicator for the learning progress
+                error = sum((visible-visibleRecon)**2)
+            print error
+            counter += 1
+            counter %= trainingData.shape[0]
+    
+    
+    # Computes sample of the learned probability distribution
+    def sample(self,numOfIteration):
+        visible = np.random.randint(0,2,self.NumOfVisibleUnits)
+        hidden = np.zeros((self.NumOfHiddenUnits,1))
+        # Sample is computed by iteratively computing the activation of hidden and visible units
+        for i in range(numOfIteration):
+            for i in range(self.NumOfHiddenUnits):
+                if np.random.random() < sigmoid(self.HiddenBiases[i] + sum(visible*self.Weights[i,:])):
+                    hidden[i] = 1
+                else:
+                    hidden[i] = 0
+                    
+            for i in range(self.NumOfVisibleUnits):
+                if np.random.random() < sigmoid(self.VisibleBiases[i] + sum(hidden*self.Weights[i,:])):
+                    visible[i] = 1
+                else:
+                    visible[i] = 0
+                    
+        return visible
